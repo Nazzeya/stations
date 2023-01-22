04b32529d96f468db68fe238125c46f099eed6ec
nazzeya <n.baisakalova01@gmail.com>
2023-01-22 16:25:42 +0600

    added model and data

---
+++ src/app/data/stations.ts
@@ -0,0 +1,28 @@
+import { IStation } from "../models/station";
+
+export const stations: IStation[] = [
+    {
+        id: 1,
+        value1: true,
+        value2: false,
+        value3: true
+    },
+    {
+        id: 2,
+        value1: false,
+        value2: false,
+        value3: true
+    },
+    {
+        id: 3,
+        value1: true,
+        value2: true,
+        value3: true
+    },
+    {
+        id: 4,
+        value1: true,
+        value2: true,
+        value3: false
+    }
+]
---
+++ src/app/models/station.ts
@@ -0,0 +1,6 @@
+export interface IStation {
+    id: number,
+    value1: boolean,
+    value2: boolean,
+    value3: boolean
+}
